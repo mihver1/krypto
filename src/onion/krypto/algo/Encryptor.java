caf065154706a6862b6e56e23ca894160201d953
Michael Verkhovykh <mihver1@gmail.com>
2012-07-24 22:02:22 +0400

    Started project. Data storage n smth more

---
+++ src/onion/krypto/algo/Encryptor.java
@@ -0,0 +1,13 @@
+package onion.krypto.algo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michael
+ * Date: 24.07.12
+ * Time: 20:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class Encryptor {
+    //placeholder
+
+}
---
+++ src/onion/krypto/algo/Morph.java
@@ -0,0 +1,31 @@
+package onion.krypto.algo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michael
+ * Date: 24.07.12
+ * Time: 20:23
+ * To change this template use File | Settings | File Templates.
+ */
+
+import onion.krypto.data.Storage;
+import onion.krypto.files.Reader;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Morph {
+    //placeholder
+    public static void main(String args[]) throws IOException {
+        System.out.println("Hello World!");
+        Reader rd = new Reader("/home/michael/Projects/krypto/data/cryptHello");
+        Storage st = rd.getStorageData();
+        try{
+            ArrayList<Integer> ali = new ArrayList<Integer>(st.getBlock());
+            System.out.print(ali.toString());
+        } finally {
+            //
+        }
+
+    }
+}
