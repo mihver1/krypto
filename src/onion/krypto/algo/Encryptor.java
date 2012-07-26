4eb8ae50f5db304d2fa4845c3680eea1a67cbe4e
Michael Verkhovykh <mihver1@gmail.com>
2012-07-27 00:31:17 +0400

    Added encryption. But decryption is still laggy. Thats why it is just hash.

--- src/onion/krypto/algo/Encryptor.java
+++ src/onion/krypto/algo/Encryptor.java
@@ -1,5 +1,14 @@
 package onion.krypto.algo;
 
+import onion.krypto.data.Storage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created with IntelliJ IDEA.
  * User: michael
@@ -7,7 +16,77 @@ package onion.krypto.algo;
  * Time: 20:26
  * To change this template use File | Settings | File Templates.
  */
+
 public class Encryptor {
-    //placeholder
-    //TODO: code encryption
+    private int[] gamma;
+
+    public void genGamma(int n) {
+        //TODO: remove random function and add user interactions
+        Random rand = new Random();
+        gamma = new int[n];
+        for (int i = 0; i < n; ++i) {
+            gamma[i] = rand.nextInt() % 256 - 128;
+        }
+        System.out.println("Gamma generated");
+    }
+
+    public void saveGamma() throws FileNotFoundException, IOException {
+        int l = gamma.length;
+        FileWriter pr = new FileWriter("/home/michael/IdeaProjects/krypto/data/cryptHello.key");
+        for (int aGamma : gamma) {
+            char c = (char) aGamma;
+            System.out.print(c);
+            pr.write(c);
+        }
+        pr.close();
+        System.out.println("Gamma saved");
+    }
+
+    public void readGamma() throws IOException {
+        FileInputStream in = null;
+        try {
+            gamma = new int[1000];
+            in = new FileInputStream("/home/michael/IdeaProjects/krypto/data/cryptHello.key");
+            int c;
+            int i = 0;
+            while ((c = in.read()) != -1) {
+                gamma[i++] = c;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
+
+    public void gammaficate(Storage st) throws FileNotFoundException, IOException {
+        ArrayList<Integer> lst = st.getBlock();
+        int i = 0;
+        FileWriter pr = new FileWriter("/home/michael/IdeaProjects/krypto/data/cryptHello.cry");
+        for (Integer aLst : lst) {
+            char c = (char) ((aLst + gamma[i++] + 128) % 256 - 128);
+            System.out.print(c);
+            pr.write(c);
+        }
+        pr.close();
+        System.out.println("Gammaficated");
+    }
+
+    public void deGammaficate(Storage st) throws FileNotFoundException, IOException {
+        ArrayList<Integer> lst = st.getBlock();
+        int i = 0;
+        FileWriter pr = new FileWriter("/home/michael/IdeaProjects/krypto/data/cryptHello.cry");
+        for (Integer aLst : lst) {
+            char c = (char) ((aLst - gamma[i++] + 128) % 256 - 128);
+            System.out.print(c);
+            pr.write(c);
+        }
+        pr.close();
+        System.out.println("Degammaficated");
+    }
+
 }
